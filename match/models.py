91a103f857a6f2cf03d0bb2e6d4d6bb0381cbed4
Daniel Wong <dawong789@yahoo.com>
2015-03-04 00:34:23 -0500

    tested model

--- match/models.py
+++ match/models.py
@@ -92,3 +92,60 @@ class Graph:
                 a.append(i)
         return a
 
+
+class Model:
+
+    # list of Person
+    people = [];
+
+    # hash of Person -> List of Person
+    exclusions = {}
+
+    def add_person(self, person):
+        self.people.append(person)
+
+    def remove_person(self, person):
+        self.people.remove(person)
+
+    def get_person(self, i):
+        return self.people[i]
+
+    def index_of(self, person):
+        return self.people.index(person)
+
+    def get_people_count(self):
+        return len(self.people)
+
+    def get_people(self):
+        return self.people[:]
+
+    def get_person_by_name(self, name):
+        for p in self.people:
+            if p.name == name:
+                return p;
+        return None;
+
+    def exclude(self, person, excludedPerson):
+        excluded = None
+        if person in self.exclusions:
+            excluded = self.exclusions[person]
+        if excluded == None:
+            excluded = []
+            self.exclusions[person] = excluded
+        excluded.append(excludedPerson)
+        # bidirectional
+        excluded = None
+        if excludedPerson in self.exclusions:
+            excluded = self.exclusions[excludedPerson]
+        if excluded == None:
+            excluded = []
+            self.exclusions[excludedPerson] = excluded
+        excluded.append(person)
+
+    def is_excluded(self, person, excludedPerson):
+        excluded = None
+        if person in self.exclusions:
+            excluded = self.exclusions[person]
+        if excluded != None:
+            return excludedPerson in excluded
+        return False;
